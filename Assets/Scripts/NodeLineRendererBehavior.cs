ef2f35c2203ddd1cc2b384adc0877b3daba7759e
zageen <zageen60@gmail.com>
2019-03-04 15:56:31 +0100

    Probuilder + Pathfinding Joueur

---
+++ Assets/Scripts/NodeLineRendererBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeLineRendererBehavior : MonoBehaviour
+{
+    private LineRenderer lineR;
+
+
+
+
+     void OnMouseDown()
+    {
+
+    } 
+}
